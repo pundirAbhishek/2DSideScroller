b725c4cebd8221c871036d7975657b3f056ee118
AbhishekPundir <abhishekpundir526@gmail.com>
2024-06-30 00:39:44 +0530

    Added falling animation

--- Assets/Scripts/PlayerCharacter2D.cs
+++ Assets/Scripts/PlayerCharacter2D.cs
@@ -8,38 +8,61 @@ public class PlayerCharacter2D : MonoBehaviour
     public SpriteRenderer sr;
     public Animator animator;
     public float jumpForce;
-    public RigidBody2D rigidBody2D;
+    public Rigidbody2D rigidBody2D;
+    bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGrounded = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        if(isGrounded)
+        {
+            animator.SetBool("isGrounded", true);
+        }
+        else
+        {
+            animator.SetBool("isGrounded", false);
+        }
+
+        // This is added so that the character can fall much faster which looks better in the game. 
+        if(rigidBody2D.velocity.y < 0)
+        {
+            rigidBody2D.velocity += Vector2.up * Physics2D.gravity.y * 2 * Time.deltaTime; 
+        }
     }
 
     void Movement()
     {
-        if(Input.getKeyDown(KeyCode.Space))
+        if(Input.GetMouseButtonDown(0))
         {
-
+            animator.SetBool("isShooting", true);
         }
 
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidBody2D.AddForce(Vector2.up * jumpForce);
+            isGrounded = false;
+            animator.SetBool("isShooting", false);
+        }
 
         if(Input.GetKey(KeyCode.D))
         {
             animator.SetBool("isWalking", true);
             transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            animator.SetBool("isShooting", false);
             sr.flipX = true;
         }
         else if(Input.GetKey(KeyCode.A))
         {
             animator.SetBool("isWalking", true);
             transform.Translate(new Vector3(-1 * speed * Time.deltaTime, 0, 0));
+            animator.SetBool("isShooting", false);
              sr.flipX = false;
         }
         else if(Input.GetKey(KeyCode.W))
@@ -55,6 +78,16 @@ public class PlayerCharacter2D : MonoBehaviour
         else
         {
             animator.SetBool("isWalking", false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("OnCollisionEnter2D");
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            animator.SetBool("isGrounded", true);
         }
     }
 }
