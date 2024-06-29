17ffbcf7757d59afb5e8bb9438ada0cf1e15e38d
AbhishekPundir <abhishekpundir526@gmail.com>
2024-06-29 20:58:49 +0530

    Added walking animation

--- Assets/Scripts/PlayerCharacter2D.cs
+++ Assets/Scripts/PlayerCharacter2D.cs
@@ -6,6 +6,10 @@ public class PlayerCharacter2D : MonoBehaviour
 {
     public int speed = 5;
     public SpriteRenderer sr;
+    public Animator animator;
+    public float jumpForce;
+    public RigidBody2D rigidBody2D;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,27 +24,37 @@ public class PlayerCharacter2D : MonoBehaviour
 
     void Movement()
     {
+        if(Input.getKeyDown(KeyCode.Space))
+        {
+
+        }
+
+
         if(Input.GetKey(KeyCode.D))
         {
+            animator.SetBool("isWalking", true);
             transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
             sr.flipX = true;
         }
         else if(Input.GetKey(KeyCode.A))
         {
+            animator.SetBool("isWalking", true);
             transform.Translate(new Vector3(-1 * speed * Time.deltaTime, 0, 0));
              sr.flipX = false;
         }
         else if(Input.GetKey(KeyCode.W))
         {
+            // animator.SetBool("isWalking", true);
             transform.Translate(new Vector3(0, speed*Time.deltaTime, 0));
         }
         else if(Input.GetKey(KeyCode.S))
         {
+            // animator.SetBool("isWalking", true);
             transform.Translate(new Vector3(0, -1 * speed * Time.deltaTime, 0));
         }
         else
         {
-
+            animator.SetBool("isWalking", false);
         }
     }
 }
